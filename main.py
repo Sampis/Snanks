f8e028e0fe4481eaa66c20d845b65e4b548fc0bb
Samuel Nilsson <Groover197@icloud.com>
2022-03-11 13:37:16 +0100

    Litt greejer

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hej")
+import sys
+
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode(1200, 600)
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill('black')
+
+    clock.tick(60)
+    pygame.display.flip()
