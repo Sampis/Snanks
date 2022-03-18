1a6571e9b89fd6cdc99ee45d3f3542bd12cb5c9a
Samuel Nilsson <Groover197@icloud.com>
2022-03-18 12:56:25 +0100

    Göra knappar barn

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ class Game:
         self.display_surface = screen
 
         self.snank = pygame.sprite.GroupSingle()
-        self.menu_font = pygame.font.SysFont("IMPACT", 60)
+        self.menu_font = pygame.font.SysFont("IMPACT", 100)
         self.intro_screen()
 
     def intro_screen(self):
@@ -15,7 +15,8 @@ class Game:
         pygame.draw.rect(self.display_surface, (255, 255, 255), (screen_width/2-200, screen_height/2+225, 400, 100), 4)
         pygame.draw.rect(self.display_surface, (255, 255, 255), (screen_width/2-200, screen_height/2+350, 400, 100), 4)
         start_text = self.menu_font.render(f"Start", 1, ("white"))
-        self.display_surface.blit(start_text, (screen_width/2, screen_height/2))
+        start_text = pygame.transform.scale(start_text, (400, 125))
+        self.display_surface.blit(start_text, (screen_width/2-200, screen_height/2+87.5))
         pygame.draw.circle(self.display_surface, ("red"), (screen_width/2, screen_height/2), 1)
 
 
